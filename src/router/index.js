d320d23bc47a8ecf5236762538d6cbc8cd3c4f36
Blade <bladerboles@gmail.com>
2020-07-24 10:16:29 -0600

    Update routes.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import ApodGenerator from '../components/ApodGenerator.vue'
 import ApodFavorites from '../components/ApodFavorites.vue'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
 
 Vue.use(VueRouter);
 
@@ -19,6 +22,36 @@ const router = new VueRouter({
       path: '/favorites',
       name: 'favorites',
       component: ApodFavorites
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('../views/Profile.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('../views/BoardAdmin.vue')
+    },
+    {
+      path: '/mod',
+      name: 'moderator',
+      component: () => import('../views/BoardModerator.vue')
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: () => import('../views/BoardUser.vue')
     }
   ]
 });
